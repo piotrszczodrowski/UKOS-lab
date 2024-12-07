e3e536beee2b3ab889115e27288eb83232ce074c
Piotr Szczodrowski <piotr.szczodrowski@pjwstk.edu.pl>
2024-12-07 08:50:24 -0500

    ls

---
+++ plik.cpp
@@ -0,0 +1,5 @@
+total 12
+drwxrwxr-x  3 kali kali 4096 Dec  7 08:45 .
+drwx------ 18 kali kali 4096 Dec  7 08:44 ..
+drwxrwxr-x  8 kali kali 4096 Dec  7 08:48 .git
+-rw-rw-r--  1 kali kali    0 Dec  7 08:45 plik.cpp
